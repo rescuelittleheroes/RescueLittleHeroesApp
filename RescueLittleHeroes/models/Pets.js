7c4feebdfb1844d8cfff5bdd051bbb35e832f9ac
Francho Garcia <franchog79@gmail.com>
2019-07-08 19:22:52 +0200

    Added pet routes in app.js, create pet route and modify pet model type property

--- RescueLittleHeroes/models/Pets.js
+++ RescueLittleHeroes/models/Pets.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PetSchema = Schema(
-  {
+const PetSchema = Schema({
     reference_id: String,
     shelter: { type: Schema.Types.ObjectId, ref: "Shelter" },
     name: String,
-    type_animal: String,
-    enum: ["Cats", "Dogs"],
+    type_animal: { type: String, enum: ["Cat", "Dog"] },
     size: { type: String, enum: ["Small", "Medium", "Big"] },
     wasFounded: Boolean,
     description: String,
@@ -15,14 +13,12 @@ const PetSchema = Schema(
     location: { type: { type: String }, coordinates: [Number] },
     neighborhood: { type: { type: String }, coordinates: [Number] },
     founded_by: { type: Schema.Types.ObjectId, ref: "User" }
-  },
-  {
+}, {
     timestamps: {
-      createdAt: "created_at",
-      updatedAt: "updated_at"
+        createdAt: "created_at",
+        updatedAt: "updated_at"
     }
-  }
-);
+});
 
 const Pet = mongoose.model("Pet", PetSchema);
 module.exports = Pet;
