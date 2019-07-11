51f674ab6873b3c294d3f91466c1b9f44513a5b4
Francho Garcia <franchog79@gmail.com>
2019-07-11 19:29:26 +0200

    console erros due google maps scripts are in layout instead of view petmaps

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,4 @@ router.get('/', (req, res, next) => {
     res.render('index');
 });
 
-router.get("/login", (req, res, next) => {
-    console.log("mandame a la ...")
-    res.redirect("/auth/login");
-});
-
-
-
-
 module.exports = router;
